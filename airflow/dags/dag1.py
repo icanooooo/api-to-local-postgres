0197a5d922bcf1c76ec50ad1ebd302c7fa1ac523
Muhammad Ihsan <muhihsan0@outlook.com>
2024-10-01 10:45:58 +0700

    created dags connected to elt script

--- airflow/dags/dag1.py
+++ airflow/dags/dag1.py
@@ -1,13 +1,26 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
+import subprocess
+
+def getData():
+    script_path = "../elt/script.py"
+    result = subprocess.run(["python", script_path],
+                            capture_output=True,
+                            text=True)
+    
+    if result.returncode != 0:
+        raise Exception(f"Script failed with error: {result.stderr}")
+    else:
+        print(result.stdout)
+
 
 def my_task():
     print("hi this is a task")
 
-with DAG('my_dag', start_date=datetime(2024, 1, 8),
+with DAG('ingestToPostgres', start_date=datetime(2024, 1, 8),
          description='Test dag', tags=['test'],
          schedule='@daily', catchup=False):
 
-    task_a = PythonOperator(task_id='Podcast_Mas', python_callable=my_task)
+    task_a = PythonOperator(task_id='moveCryptoSQL', python_callable=getData)
     
