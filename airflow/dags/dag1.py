8f87a7ecb1700f24127ca4f2e8e39d123c82e7d9
Muhammad Ihsan <muhihsan0@outlook.com>
2024-10-14 09:28:37 +0700

    edited dag to make more readible airflow tasks

--- airflow/dags/dag1.py
+++ airflow/dags/dag1.py
@@ -2,26 +2,99 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 import subprocess
-import os
+import requests
+import psycopg2
+import time
+import json
 
-def getData():
-    script_path = "/opt/airflow/elt/script.py"
-    result = subprocess.run(["python", script_path],
-                            capture_output=True,
-                            text=True)
+def fetch_api():
+    headers = {
+            'Accept':'application/json',
+            'Content-Type': 'application/json'
+    }
     
-    if result.returncode != 0:
-        raise Exception(f"Script failed with error: {result.stderr}")
+    url = "https://api.coincap.io/v2/assets"
+    response = requests.request("GET", url, headers=headers, data=())
+
+    if response.status_code == 200: #Connected Let's go:
+        print("Sucessfully Connected to API")
+        myjson = response.json()
+
+        return myjson
     else:
-        print(result.stdout)
+        raise Exception(f"womp,womp: {response.status_code}")
 
+def load_to_db(**kwargs):
+    max_retries=5
+    delay=10
+    tries=0
 
-def my_task():
-    print("hi this is a task")
+    while tries < max_retries:
+        try:
+            connection = psycopg2.connect(
+                host="destination_pgres",
+                database="destination_db",
+                user="icanooo",
+                password="secret"
+            )
+            print("CONNECTED")
+            break
+        except:
+            tries += 1
+            print('retrying..')
+            time.sleep(15)
+
+    try:
+        ti = kwargs['ti']
+
+        cursor = connection.cursor()
+
+        create_table = '''
+        CREATE TABLE IF NOT EXISTS crypto_table (
+            id SERIAL PRIMARY KEY,
+            symbol VARCHAR(50),
+            name VARCHAR(50),
+            price_in_usd DOUBLE PRECISION,
+            supply FLOAT,
+            max_supply FLOAT,
+            insert_date TIMESTAMP
+        );
+        '''
+        cursor.execute(create_table)
+        print("Table created <3")
+
+        insert_query ='''
+        INSERT INTO crypto_table (symbol, name, price_in_usd, supply, max_supply, insert_date)
+        values (%s, %s, %s, %s, %s, %s);
+        '''
+
+        api_pull = ti.xcom_pull(task_ids='ingest_api')
+        data = api_pull['data']
+        
+        for item in data:
+            cursor.execute(insert_query, (item['symbol'], item['name'], 
+                                          float(item['priceUsd']) if item['priceUsd'] else None,
+                                          float(item['supply']) if item['supply'] else None,
+                                          float(item['maxSupply']) if item['maxSupply'] else None,
+                                          datetime.now()
+                                          )
+                                        )
+            print(f"inserting {item['name']}")
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    except psycopg2.Error as e:
+        print(f"Error due to: {e}")
+        return False
+
 
 with DAG('ingestToPostgres', start_date=datetime(2024, 1, 8),
          description='Test dag', tags=['test'],
          schedule='@daily', catchup=False):
 
-    task_a = PythonOperator(task_id='moveCryptoSQL', python_callable=getData)
+    task_a = PythonOperator(task_id='ingest_api', python_callable=fetch_api)
+    task_b = PythonOperator(task_id='inserting_db', python_callable=load_to_db)
     
+    task_a >> task_b
